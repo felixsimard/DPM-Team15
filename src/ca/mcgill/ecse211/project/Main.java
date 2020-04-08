1d9ee3aaddccd4afc22df9b7a81e559ed0093de7
Mohammad Noor <mohammad.noor@mail.mcgill.ca>
2020-04-08 13:44:03 -0400

    updated java docs

--- src/ca/mcgill/ecse211/project/Main.java
+++ src/ca/mcgill/ecse211/project/Main.java
@@ -5,13 +5,18 @@ import lejos.hardware.Button;
 
 /**
 * The main driver class for the search and rescue procedure.
+* This class is where the search and rescue procedure is started - in the main(String[] args) method
 */
 public class Main {
 
 //-----------------------------------------------------------------------------------------------------------------------  
 /**
  * The main entry point.
- * 
+ * This method is where the search and rescue procedure is initialized.
+ * Thread initializations including the odometer,display, ultrasonic sensor, leftwheel wheel colour sensor and the right wheel
+ * colour sensor are done here.
+ * The method usLocalize() - performs ultrasonic localization -  from the UltrasonicLocalizer class is also called from here.
+ * The rest of the search and rescue process is done in a sequential manner so methods are called from appropriate classes when necessary
  * @param args not used
  */
 public static void main(String[] args) {
